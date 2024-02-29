9423814c130ebd789ae6ea049b883c7f9c56b607
Bernardo Stearns <bernardostearns@DSIs-MacBook-Pro-3.local>
2024-02-29 20:31:41 +0000

    removed multiple masks llm sentence, became to complicated normalize predictions. need to add str label to plot

--- selva-agreement-clients/poetry-client/client.py
+++ selva-agreement-clients/poetry-client/client.py
@@ -41,9 +41,27 @@ def main():
                             token_idx,
                             nMasks=maxNumOfMasks,
                             )
+                llm_masked_sentence = llm_masked_sentences[0]
                 sentences_predictions = dataset.fill_masks_pipeline(llm_masked_sentences)
+                llm_masked_sentence_predictions = dataset.fill_masks_pipeline([llm_masked_sentence]) 
+                k=3
+                print(''.join(llm_masked_sentence))
+                print(len(llm_masked_sentence_predictions))
+                top_k_probs = [str(rag['score']) for rag in llm_masked_sentence_predictions[:k]]
+                top_k_str = [rag['token_str'] for rag in llm_masked_sentence_predictions[:k]]
+                top_k_labels = [i+1 for i in range(k)]
+
+                plt.simple_bar(top_k_labels,
+                               top_k_probs, 
+                               width = 100,
+                               title = f'model top {k}')
+                plt.show() 
+                input()
+                '''
                 for s_idx, sentence_predictions in enumerate(sentences_predictions):
                     k=3
+                    print(''.join(llm_masked_sentences[s_idx]))
+                    print(len(sentence_predictions))
                     top_k_probs = [rag['score'] for rag in sentence_predictions[:k]]
                     top_k_labels = [i+1 for i in range(k)]
 
@@ -52,9 +70,7 @@ def main():
                                    width = 100,
                                    title = f'model top {k}')
                     plt.show() 
-                    print(''.join(llm_masked_sentences[s_idx]))
                     input()
-                    '''
                     for p in sentence_predictions:
                         print(f'token: {p["token_str"]} {p["score"]}')
                         print('\n')
